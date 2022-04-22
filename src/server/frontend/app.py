e1446566e1ce5f29850d67a620c6c828f98a1fa1
NikitaZotov <nikita.zotov.belarus@gmail.com>
2022-04-22 22:50:45 +0300

    feat(logger): log in file

--- src/server/frontend/app.py
+++ src/server/frontend/app.py
@@ -5,15 +5,19 @@
 from flask import Flask
 from flask_cors import CORS
 
+from log import get_default_logger
 from ..frontend.configurator import BaseConfigurator
 
 cors = CORS()
+
+logger = get_default_logger(__name__)
 
 
 class Application(Flask):
     def __init__(self):
         super().__init__(__name__, template_folder='templates')
         cors.init_app(self)
+        self.logger = logger
 
     def start(self, configurator: BaseConfigurator) -> None:
         self.prepare()
