df30bb88e5143396396a6206a50339048fc24ab6
NikitaZotov <nikita.zotov.belarus@gmail.com>
2022-04-22 19:24:14 +0300

    feat(routes): add exception clauses processing

--- src/server/frontend/routes.py
+++ src/server/frontend/routes.py
@@ -1,8 +1,10 @@
 """
     Author Zotov Nikita
 """
+from http import HTTPStatus
 
 from flask import render_template, send_from_directory, request, jsonify
+from werkzeug.exceptions import abort
 
 from server.frontend.app import Application
 from server.frontend.configurator import Configurator
@@ -13,9 +15,6 @@ app = Application(configurator)
 dir_path = ""
 
 
-# abort(HTTPStatus.BAD_REQUEST, description=status.value)
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -24,23 +23,27 @@ def index():
 @app.route('/download')
 def download_file():
     file_name = request.args.get("file")
+    if not file_name:
+        return jsonify(error="Empty file name"), HTTPStatus.BAD_REQUEST
 
-    return send_from_directory(dir_path, file_name)
+    return send_from_directory(dir_path, file_name, error="File or directory not found")
 
 
 @app.route('/set', methods=['POST'])
 def set_directory():
     global dir_path
     dir_path = request.args.get("directory")
+    if not dir_path:
+        return jsonify(error="Empty directory name"), HTTPStatus.BAD_REQUEST
 
     return jsonify(dir_path=dir_path)
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html', error=error), 404
+    return jsonify(error=str(error)), HTTPStatus.BAD_REQUEST
 
 
 @app.errorhandler(500)
 def internal_server_error(error):
-    return render_template("500.html", error=error), 500
+    return jsonify(error=str(error)), HTTPStatus.INTERNAL_SERVER_ERROR
