bf56d29a5f167547e568ddc69fe04289eb1340bc
NikitaZotov <nikita.zotov.belarus@gmail.com>
2022-05-10 15:20:43 +0300

    feat(query-service): fix sentences visiting

--- src/app/services/query/parser.py
+++ src/app/services/query/parser.py
@@ -19,7 +19,8 @@ class ScsQueryParser:
         self.var_prefix = "_"
 
     def parse(self, query: str) -> Tuple[ScTemplate, List[str]]:
-        sentences = query.replace(" ", "").split(self.semicolon)
+        sentences = self._tokenize(query)
+        print(sentences)
 
         edge_number = 0
         template = ScTemplate()
@@ -31,13 +32,14 @@ class ScsQueryParser:
                 nodes = sentence.split(self.var_access_edge)
 
                 source, target = nodes[0], nodes[1]
-                params.extend([source, target])
 
                 template.triple(
-                    self._resolve_param(source),
+                    self._resolve_param(source, params),
                     [sc_types.EDGE_ACCESS_VAR_POS_PERM, edge_alias],
-                    self._resolve_param(target),
+                    self._resolve_param(target, params),
                 )
+
+                params.extend([source, target])
             elif sentence.count(self.var_common_edge) == 1:
                 nodes = sentence.split(self.var_common_edge)
 
@@ -45,22 +47,35 @@ class ScsQueryParser:
 
                 nodes = nodes.split(self.var_relation)
                 relation, target = nodes[0], nodes[1]
-                params.extend([source, relation, target])
+                print(params)
 
                 template.triple_with_relation(
-                    self._resolve_param(source),
+                    self._resolve_param(source, params),
                     [sc_types.EDGE_D_COMMON_VAR, edge_alias],
-                    self._resolve_param(target),
+                    self._resolve_param(target, params),
                     [sc_types.EDGE_ACCESS_VAR_POS_PERM, common_edge_alias],
-                    self._resolve_param(relation)
+                    self._resolve_param(relation, params)
                 )
+
+                params.extend([source, relation, target])
 
             edge_number += 2
 
         return template, params
 
+    def _tokenize(self, query: str) -> List[str]:
+        sentences = query.replace(" ", "")
+        sentences = sentences.replace("\r", "")
+        sentences = sentences.replace("\n", "")
+
+        return sentences.split(self.semicolon)
+
     def _is_var(self, node: str) -> bool:
         return node.startswith(self.var_prefix)
 
-    def _resolve_param(self, node: str):
-        return [sc_types.UNKNOWN, node] if self._is_var(node) else self._keynodes[node]
+    def _resolve_param(self, node: str, params: List[str]):
+        if node in params:
+            print("in params")
+            return node if self._is_var(node) else self._keynodes[node]
+        else:
+            return [sc_types.UNKNOWN, node] if self._is_var(node) else self._keynodes[node]
